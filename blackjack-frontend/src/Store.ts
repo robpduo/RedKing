e6d2cc3cacd8ca0fbabf10148fd63a97255da3c4
nabilgj <nabilgj.bnm@gmail.com>
2022-05-28 12:13:46 -0400

    did some cleanup in the app, index, and slices

--- blackjack-frontend/src/Store.ts
+++ blackjack-frontend/src/Store.ts
@@ -1,18 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 
 //We would import our reducers from the slices we have to create
 // import userReducer from "./Slices/UserSlice";
 // import reimburseReducer from "./Slices/ReimbursementSlice";
 // import employeesReducer from "./Slices/EmployeesSlice";
 
-export const Store = configureStore({
-    reducer: {
-        // user: userReducer,
-        // reimburser: reimburseReducer,
-        // employees: employeesReducer
-    }
-});
+// export const Store = configureStore({
+//   reducer: {
+//     user: userReducer,
+//     reimburser: reimburseReducer,
+//     employees: employeesReducer
+//   },
+// });
 
 //We must export these two things to make our lives easier later
-export type RootState = ReturnType<typeof Store.getState>;
-export type AppDispatch = typeof Store.dispatch;
+// export type RootState = ReturnType<typeof Store.getState>;
+// export type AppDispatch = typeof Store.dispatch;
