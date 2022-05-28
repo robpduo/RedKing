86267fff82197d1bda8eeca44c660c47c216441b
robpduo <101683611+robpduo@users.noreply.github.com>
2022-05-28 16:40:09 -0400

    Rest Controller and Cross Origin

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -10,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 public class UserController {
 
