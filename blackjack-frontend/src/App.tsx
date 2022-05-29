05d5bcab51ab268af235d21e7c00aa5fad90825c
nabilgj <nabilgj.bnm@gmail.com>
2022-05-29 13:53:10 -0400

    created a register page without css

--- blackjack-frontend/src/App.tsx
+++ blackjack-frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { HomeScreen } from './Views/HomeScreen/HomeScreen';
 import { LoginPage } from './Views/LoginPage/LoginPage';
+import { RegisterPage } from './Views/RegisterPage/RegisterPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="*" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<HomeScreen />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/playgame" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
