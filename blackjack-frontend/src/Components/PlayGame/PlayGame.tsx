40dffc9f3149cbc0507d2ebce457fe40b05b4210
zamanuap <zaman_uap@yahoo.com>
2022-06-05 16:43:12 -0400

    get mail on winning

--- blackjack-frontend/src/Components/PlayGame/PlayGame.tsx
+++ blackjack-frontend/src/Components/PlayGame/PlayGame.tsx
@@ -18,6 +18,7 @@ import {
 import { quitGame } from '../../Slices/DeckSlice';
 import { StandButton } from '../StandButton/StandButton';
 import NextRound from '../NextRound/NextRound';
+import { sendMail } from '../../Slices/UserSlice';
 
 // going inside PlaGamePage
 export const PlayGame: React.FC<IDeck> = (deck: IDeck) => {
@@ -32,7 +33,7 @@ export const PlayGame: React.FC<IDeck> = (deck: IDeck) => {
   const playerCards = useSelector((state: RootState) => state.deck.playerHand);
 
   const dealerCards = useSelector((state: RootState) => state.deck.dealerHand);
-
+  const userState = useSelector((state:RootState) => state.user.user);
   console.log('coming from PlayGame line 36', gameState.winner);
 
   const handleScoreBoard = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -45,6 +46,20 @@ export const PlayGame: React.FC<IDeck> = (deck: IDeck) => {
     dispatch(setWinner('none'));
     dispatch(togglePlayerBusted());
   };
+
+  useEffect(() => {
+    if(userState){
+      let mailData = {
+        firstName: userState?.firstName,
+        email: userState?.email,
+        msgType: "Win"
+      }
+
+      if(gameState.winner !== 'none' && gameState.winner !== 'dealer'){
+          dispatch(sendMail(mailData))
+      }
+    }    
+  },[gameState.winner]);
 
   return (
     <>
